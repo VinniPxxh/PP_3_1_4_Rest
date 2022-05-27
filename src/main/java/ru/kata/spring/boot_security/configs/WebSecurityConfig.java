8c2ab4e381c0d39026015fa028f240c89dad3fd0
VinniPxxh <kuznetsove2001@mail.ru>
2022-05-27 19:29:12 +0300

    fix packages

--- src/main/java/ru/kata/spring/boot_security/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/configs/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.kata.spring.boot_security.demo.configs;
+package ru.kata.spring.boot_security.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.kata.spring.boot_security.service.UserService;
 
 @Configuration
 @EnableWebSecurity
