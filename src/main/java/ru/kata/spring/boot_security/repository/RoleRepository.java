ea4788a8c6226ed230794e0235c5cf818b6a5a30
VinniPxxh <kuznetsove2001@mail.ru>
2022-05-30 20:56:36 +0300

    final commit

--- src/main/java/ru/kata/spring/boot_security/repository/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/repository/RoleRepository.java
@@ -2,7 +2,6 @@ package ru.kata.spring.boot_security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kata.spring.boot_security.model.Role;
-import ru.kata.spring.boot_security.model.User;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
