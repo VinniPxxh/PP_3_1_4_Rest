bf49753cdbd4d950a8204a6497751575ef5bb213
VinniPxxh <kuznetsove2001@mail.ru>
2022-05-31 21:16:26 +0300

    fix for java mentor :D

--- src/main/java/ru/kata/spring/boot_security/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package ru.kata.spring.boot_security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.kata.spring.boot_security.model.User;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+//    @Query("SELECT DISTINCT user FROM User user " +    - не знаю, как это должно работать,
+//            "JOIN FETCH user.roles roles")               не могу разобраться.
+//    List<User> retrieveAll();
 
 }
