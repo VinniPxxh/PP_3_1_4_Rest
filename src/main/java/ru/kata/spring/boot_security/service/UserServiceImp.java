cf0c8847fa51bf2ac449dbeb84a0438e113dba20
VinniPxxh <kuznetsove2001@mail.ru>
2022-06-02 11:55:56 +0300

    Fix UserServiceImp

--- src/main/java/ru/kata/spring/boot_security/service/UserServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/service/UserServiceImp.java
@@ -17,8 +17,6 @@ import java.util.Set;
 
 @Service
 public class UserServiceImp implements UserDetailsService, UserService {
-
-    private User user;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
@@ -28,35 +26,38 @@ public class UserServiceImp implements UserDetailsService, UserService {
         this.roleRepository = roleRepository;
     }
     @Transactional
+    @Override
     public void addOrUpdateUser(User user, Set<Role> roles) {
         user.setRoles(roles);
         userRepository.save(user);
     }
 
-
+    @Override
     public List<User> findAll() {
         return userRepository.findAll();
     }
-
+    @Override
     public User getUserById(Long id) {
         return userRepository.findById(id).get();
     }
-
+    @Override
     public List<Role> listRoles() {
         return roleRepository.findAll();
     }
     @Transactional
+    @Override
     public void deleteById(User user) {
         userRepository.delete(user);
     }
-
+    @Override
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+
+
     }
 }
